92a3a878926aea17fb6f57fee4a32f8bfc8d3891
Dan2458 <89513702+Dan2458@users.noreply.github.com>
2022-11-10 21:52:28 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    
+    <BootcampRegister />
+  </div>
+</template>
+
+<script>
+import BootcampRegister from './components/BootcampRegister.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BootcampRegister
+  }
+}
+</script>
+
+
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+import './assets/tailwind.css'
+import Vuelidate from 'vuelidate'
+
+Vue.use(Vuelidate)
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
